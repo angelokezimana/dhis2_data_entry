13fb572fbbfcd0ef991266587dfc4d7a23e37759
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-09-23 17:03:18 +0200

    feat: display data entry for HIV Care Monthly data set (WIP)

--- app/Http/Controllers/DataEntryController.php
+++ app/Http/Controllers/DataEntryController.php
@@ -13,7 +13,58 @@ class DataEntryController extends Controller
      */
     public function index()
     {
-        return view('dataentries.index');
+        $dataset = 'vc6nF5yZsPR';
+        $period = '202306';
+
+        $data_set = DB::table('organisation_units')
+            ->join('data_set_organisation', 'organisation_units.id', '=', 'data_set_organisation.org_id')
+            ->join('data_sets', 'data_set_organisation.data_set_id', '=', 'data_sets.id')
+            ->join('data_elements', 'data_sets.id', '=', 'data_elements.data_set_id')
+            ->join('hiv_testings', 'data_elements.id', '=', 'hiv_testings.data_element_id')
+            ->where('data_sets.id', '=', $dataset)
+            ->where(DB::raw("DATE_FORMAT(hiv_testings.created_at, '%Y%m')"), '=', $period)
+            ->select(
+                'organisation_units.id AS orgUnitId',
+                'organisation_units.display_name AS orgUnit',
+                'data_sets.id AS dataSetId',
+                'data_sets.display_name AS dataSet',
+                DB::raw("DATE_FORMAT(hiv_testings.created_at, '%Y%m') AS period")
+            )
+            ->distinct()
+            ->first();
+
+        $data_values = DB::table('data_sets')
+            ->join('data_elements', 'data_sets.id', '=', 'data_elements.data_set_id')
+            ->join('hiv_testings', 'data_elements.id', '=', 'hiv_testings.data_element_id')
+            ->join('category_option_combos', 'hiv_testings.category_option_combo_id', '=', 'category_option_combos.id')
+            ->where('data_sets.id', '=', $dataset)
+            ->where(DB::raw("DATE_FORMAT(hiv_testings.created_at, '%Y%m')"), '=', $period)
+            ->select(
+                'data_elements.display_name AS dataElement',
+                'category_option_combos.display_name AS categoryOptionCombo',
+                DB::raw('COUNT(hiv_testings.patient_id) AS value'),
+            )
+            ->groupBy('dataElement', 'categoryOptionCombo')
+            ->get();
+
+        $table_data_values = [];
+
+        foreach ($data_values as $data_value) {
+            $data_element = $data_value->dataElement;
+            $category_option_combo = $data_value->categoryOptionCombo;
+            $value = $data_value->value;
+
+            if (!isset($table_data_values[$data_element])) {
+                $table_data_values[$data_element] = [];
+            }
+
+            $table_data_values[$data_element][$category_option_combo] = $value;
+        }
+        // dd($table_data_values, $data_values);
+        return view('dataentries.index', [
+            'data_set' => $data_set,
+            'data_values' => $table_data_values
+        ]);
     }
 
     /**
--- resources/views/dataentries/index.blade.php
+++ resources/views/dataentries/index.blade.php
@@ -9,7 +9,42 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    index data values
+                    <table>
+                        <tr>
+                            <td>Organisation Unit</td>
+                            <td>: {{ $data_set->orgUnit }}</td>
+                        </tr>
+                        <tr>
+                            <td>Data Set</td>
+                            <td>: {{ $data_set->dataSet }}</td>
+                        </tr>
+                        <tr>
+                            <td>Period</td>
+                            <td>: {{ $data_set->period }}</td>
+                        </tr>
+                    </table>
+                    <table class="border-collapse border">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th class="border">&lt;15y</th>
+                                <th class="border">15-24y</th>
+                                <th class="border">25-49y</th>
+                                <th class="border">&gt;49y</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($data_values as $data_element => $values)
+                            <tr>
+                                <td class="border">{{ $data_element }}</td>
+                                <td class="border">{{ $values['<15y'] ?? 0 }}</td>
+                                <td class="border">{{ $values['15-24y'] ?? 0 }}</td>
+                                <td class="border">{{ $values['25-49y'] ?? 0 }}</td>
+                                <td class="border">{{ $values['>49y'] ?? 0 }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
