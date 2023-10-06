20e9c762d1e7f65a9e71a42cdab28003a53cd505
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-10-06 23:57:38 +0200

    style: improve design

--- resources/views/livewire/get-data-set.blade.php
+++ resources/views/livewire/get-data-set.blade.php
@@ -3,9 +3,11 @@
         @csrf
         <table>
             <tr>
-                <td>Organisation Unit</td>
-                <td>:
-                    <select wire:model="org" wire:click="getDataSets" class="border border-pink-500">
+                <td class="pr-4">
+                    <label for="org" class="text-sm leading-4 font-medium text-gray-500">Organisation Unit</label>
+                </td>
+                <td>
+                    <select id="org" wire:model="org" wire:click="getDataSets" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
                         <option value="">Choose an organisation unit</option>
                         @foreach($organisations as $org_unit)
                             <option value="{{ $org_unit->id }}">{{ $org_unit->display_name }}</option>
@@ -15,9 +17,11 @@
             </tr>
             @if(count($datasets) > 0)
                 <tr>
-                    <td>Data Set</td>
-                    <td>: 
-                        <select name="dataset" wire:click="getPeriods" wire:model="dataset" class="border border-pink-500">
+                    <td class="pr-4">
+                        <label for="dataset" class="text-sm leading-4 font-medium text-gray-500">Data Set</label>
+                    </td>
+                    <td>
+                        <select id="dataset" name="dataset" wire:click="getPeriods" wire:model="dataset" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
                             <option value="">Choose a dataset</option>
                             @foreach($datasets as $data_set)
                                 <option value="{{ $data_set->dataSetId }}">{{ $data_set->dataSet }}</option>
@@ -28,9 +32,11 @@
             @endif
             @if(count($periods) > 0)
                 <tr>
-                    <td>Period</td>
-                    <td>: 
-                        <select name="period" wire:click="getDataValues" wire:model="period" class="border border-pink-500">
+                    <td class="pr-4">
+                        <label for="period" class="text-sm leading-4 font-medium text-gray-500">Period</label>
+                    </td>
+                    <td>
+                        <select id="period" name="period" wire:click="getDataValues" wire:model="period" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
                             <option value="">Choose a period</option>
                             @foreach($periods as $item)
                                 <option value="{{ $item->period }}">{{ $item->DisplayPeriod }}</option>
@@ -42,29 +48,39 @@
         </table>
 
         @if(count($dataValues) > 0)
-            <table class="border-collapse border">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th class="border">&lt;15y</th>
-                        <th class="border">15-24y</th>
-                        <th class="border">25-49y</th>
-                        <th class="border">&gt;49y</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($this->dataValues as $data_element => $values)
-                    <tr>
-                        <td class="border">{{ $data_element }}</td>
-                        <td class="border">{{ $values['<15y'] ?? 0 }}</td>
-                        <td class="border">{{ $values['15-24y'] ?? 0 }}</td>
-                        <td class="border">{{ $values['25-49y'] ?? 0 }}</td>
-                        <td class="border">{{ $values['>49y'] ?? 0 }}</td>
-                    </tr>
+        <div class="flex flex-col">
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                  <table class="min-w-full text-left text-sm font-light">
+                    <thead class="border-b font-medium dark:border-neutral-500">
+                      <tr>
+                        <th scope="col" class="px-6 py-4"></th>
+                        <th scope="col" class="px-6 py-3">&lt;15y</th>
+                        <th scope="col" class="px-6 py-3">15-24y</th>
+                        <th scope="col" class="px-6 py-3">25-49y</th>
+                        <th scope="col" class="px-6 py-3">&gt;49y</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($this->dataValues as $data_element => $values)
+                       <tr class="border-b dark:border-neutral-500">
+                           <td class="whitespace-nowrap px-6 py-4">{{ $data_element }}</td>
+                           <td class="whitespace-nowrap px-6 py-4">{{ $values['<15y'] ?? 0 }}</td>
+                           <td class="whitespace-nowrap px-6 py-4">{{ $values['15-24y'] ?? 0 }}</td>
+                           <td class="whitespace-nowrap px-6 py-4">{{ $values['25-49y'] ?? 0 }}</td>
+                           <td class="whitespace-nowrap px-6 py-4">{{ $values['>49y'] ?? 0 }}</td>
+                       </tr>
                     @endforeach
-                </tbody>
-            </table>
-            <button class="border border-slate-600" type="submit">Envoyer</button>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+        </div>
+        <button type="submit" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Envoyer
+        </button>
         @endif
     </form>
 </div>
