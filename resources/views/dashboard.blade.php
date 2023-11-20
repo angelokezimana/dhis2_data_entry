a7ee29980990121300dbb628f088192c0ad62237
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-11-20 22:46:32 +0200

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 text-gray-700 text-justify mx-20">
+                    {{ __("This project is an educational website focused on interacting with the DHIS2 platform, specifically
+                    targeting the HIV Care Monthly dataset.
+                    The goal is to provide a learning resource for individuals interested in working with DHIS2 APIs and
+                    gaining hands-on experience with the HIV Care Monthly data.") }}
                 </div>
             </div>
         </div>
