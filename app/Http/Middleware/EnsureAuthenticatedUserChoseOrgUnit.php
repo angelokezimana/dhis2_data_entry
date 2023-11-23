bc502d14955a8beacb136f49469d57436e67bf3f
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-11-23 23:11:52 +0200

    feat: create EnsureAuthenticatedUserChoseOrgUnit middleware

---
+++ app/Http/Middleware/EnsureAuthenticatedUserChoseOrgUnit.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\OrganisationUnit;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureAuthenticatedUserChoseOrgUnit
+{
+    private function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->session()->has('org_unit')) {
+            $this->logout($request);
+            return redirect('/');
+        }
+        
+        $org_unit = OrganisationUnit::find($request->session()->get('org_unit'));
+        
+        if (!$org_unit) {
+            $this->logout($request);
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
