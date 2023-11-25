19c52a815e5e7f4d79c90372b93d6f6656d6096b
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-11-25 20:35:19 +0200

    chore: remove unused files

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Patient;
-use App\Http\Requests\StorePatientRequest;
-use App\Http\Requests\UpdatePatientRequest;
 
 class PatientController extends Controller
 {
@@ -17,41 +15,9 @@ class PatientController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePatientRequest $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePatientRequest $request, Patient $patient)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Patient $patient)
     {
         //
     }
