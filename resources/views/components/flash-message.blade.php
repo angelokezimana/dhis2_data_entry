4296b711a012726d33d8120169031b1369f3b3fe
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-11-26 22:14:59 +0200

    style: format the code

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,8 +1,8 @@
-@if(session()->has('is_success'))
-<div class="{{ session('is_success') === true ? 
-            'bg-green-200 border border-green-400 text-green-700' : 
-            'bg-red-100 border border-red-400 text-red-700' }} px-4 py-3 rounded relative" 
-    role="alert">
-    <span class="block sm:inline">{{ session('response') }}</span>
-</div>
+@if (session()->has('is_success'))
+    <div class="{{ session('is_success') === true
+        ? 'bg-green-200 border border-green-400 text-green-700'
+        : 'bg-red-100 border border-red-400 text-red-700' }} px-4 py-3 rounded relative"
+        role="alert">
+        <span class="block sm:inline">{{ session('response') }}</span>
+    </div>
 @endif
