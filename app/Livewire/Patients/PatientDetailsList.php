3d176397f64c5976c4fe0a6f45f33ae2d980db9d
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-11-26 22:15:10 +0200

    feat: create PatientDetailsForm and PatientDetailsList livewire components

---
+++ app/Livewire/Patients/PatientDetailsList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Patients;
+
+use Livewire\Component;
+
+class PatientDetailsList extends Component
+{
+    public function render()
+    {
+        return view('livewire.patients.patient-details-list');
+    }
+}
--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Patient details') }}
+            {{ __('Patient details') }} - {{ $patient->full_name }}
         </h2>
     </x-slot>
 
@@ -9,13 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row gap-2">
             <div class="mb-3 bg-white overflow-hidden shadow-sm sm:rounded-lg basis-1/3">
                 <div class="p-6 text-gray-900">
-                    {{ $patient->full_name }}
+                    @include('components.flash-message')
+                    <livewire:patients.patient-details-form />
                 </div>
             </div>
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg basis-2/3">
                 <div class="p-6 text-gray-900">
-                    
+                    <livewire:patients.patient-details-list />
                 </div>
             </div>
         </div>
